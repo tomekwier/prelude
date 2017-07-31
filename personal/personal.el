5cccb2ead7b324e8266526b4d76ba86fff42eaef
Tomek Wiercioch <tomek@none>
2017-07-31 08:34:59 +0200

    Add dependency to bookmark+

--- personal/personal.el
+++ personal/personal.el
@@ -12,6 +12,7 @@
 (setq package-list '(
 		     exec-path-from-shell
 		     go-mode
+                     bookmark+
                      go-errcheck
                      go-guru
                      go-gopath
@@ -87,6 +88,8 @@
 ;; Fix ansi-term and yasnippet
 (add-hook 'term-mode-hook (lambda()
                             (setq yas-dont-activate t)))
+;; Bookmark++
+(require 'bookmark+)
 
 ;; Enable company mode
 (require 'company)
