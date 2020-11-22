0c6fefe6b7cf3e2f89444d0d57ffa00bd89b2fe7
Tomek Wiercioch <tomek@mail.com>
2020-11-22 19:34:53 +0100

    Cleanup lsp configuration

--- personal/personal-lsp.el
+++ personal/personal-lsp.el
@@ -2,15 +2,12 @@
 (setq lsp-keymap-prefix "s-l")
 
 (use-package lsp-mode
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+  :hook (
          (typescript-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
-;; optionally
 (use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
 (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
